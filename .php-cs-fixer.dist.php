68892bc0d5ebdb0f9fb3f47b2f2d9837e53ecca2
dazz <annejulia@gmail.com>
2025-03-23 08:52:15 +0100

    chore(cs): update CS-Fixer config with snake_case test method naming rule

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -5,7 +5,9 @@ use PhpCsFixer\Finder;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
 
 $finder = (new Finder())
-    ->in(__DIR__);
+    ->in(__DIR__ . '/src')
+    ->in(__DIR__ . '/tests')
+;
 
 // Custom rule for test method names in snake_case
 $snakeCaseTestMethodsRules = [
@@ -18,48 +20,15 @@ return (new Config())
     ->setParallelConfig(ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
+        'yoda_style' => false, // overrides @Symfony rule set
+        'global_namespace_import' => false, // overrides @Symfony rule set
+        'concat_space' => ['spacing' => 'one'], // overrides @Symfony rule set
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'nullable_type_declaration' => true,
+        'nullable_type_declaration_for_default_null_value' => true, // overrides @Symfony rule set
         'heredoc_indentation' => ['indentation' => 'start_plus_one'],
         'php_unit_method_casing' => ['case' => 'snake_case'],
     ])
-    ->registerCustomFixers([
-        new class() extends \PhpCsFixer\AbstractFixer {
-            public function getDefinition(): \PhpCsFixer\FixerDefinition\FixerDefinitionInterface
-            {
-                return new \PhpCsFixer\FixerDefinition\FixerDefinition(
-                    'Test method names must be in snake_case and start with test_.',
-                    []
-                );
-            }
 
-            public function isCandidate(\PhpCsFixer\Tokenizer\Tokens $tokens): bool
-            {
-                return $tokens->isAllTokenKindsFound([\T_CLASS, \T_FUNCTION]);
-            }
-
-            protected function applyFix(\SplFileInfo $file, \PhpCsFixer\Tokenizer\Tokens $tokens): void
-            {
-                if (!str_contains($file->getPathname(), 'Test.php')) {
-                    return;
-                }
-
-                for ($index = 0, $count = $tokens->count(); $index < $count; ++$index) {
-                    if (!$tokens[$index]->isGivenKind(\T_FUNCTION)) {
-                        continue;
-                    }
-
-                    $nameIndex = $tokens->getNextMeaningfulToken($index);
-                    if (!$tokens[$nameIndex]->isGivenKind(\T_STRING)) {
-                        continue;
-                    }
-
-                    $methodName = $tokens[$nameIndex]->getContent();
-                    if (str_starts_with($methodName, 'test') && !str_starts_with($methodName, 'test_')) {
-                        $newName = 'test_' . preg_replace('/^test/', '', lcfirst(preg_replace('/(?<=\\w)(?=[A-Z])/', '_$1', $methodName)));
-                        $newName = strtolower($newName);
-                        $tokens[$nameIndex] = new \PhpCsFixer\Tokenizer\Token([\T_STRING, $newName]);
-                    }
-                }
-            }
-        },
-    ])
     ->setFinder($finder);
