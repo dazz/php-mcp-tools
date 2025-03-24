24e7701dd33dca5839065b686a1e3941bef631e8
Anne-Julia Seitz <anne.seitz@qossmic.com>
2025-03-24 22:40:18 +0100

    fix(random): we need string args and string return for now
    there are still things to figure out in the mcp-sdk

--- src/Tool/Random.php
+++ src/Tool/Random.php
@@ -5,14 +5,29 @@ declare(strict_types=1);
 namespace Dazz\PhpMcpTools\Tool;
 
 use Dazz\PhpMcpTools\Exception\InvalidArgumentException;
-use PhpLlm\LlmChain\Chain\Toolbox\Attribute\AsTool;
+use PhpLlm\LlmChain\Chain\JsonSchema\Attribute\With;
+use PhpLlm\LlmChain\Chain\ToolBox\Attribute\AsTool;
 use Random\RandomException;
 
 #[AsTool(name: 'random_number', description: 'Get a random number between min and max', method: 'number')]
 final readonly class Random
 {
-    public function number(int $min = 0, int $max = PHP_INT_MAX): int
-    {
+    /**
+     * @param string $min minimum number (default: 0)
+     * @param string $max maximum number (default: 9223372036854775807)
+     *
+     * @return string
+     */
+    public function number(
+        #[With(pattern: '/^[0-9]+$/')]
+        string $min = '0',
+        #[With(pattern: '/^[0-9]+$/')]
+        string $max = '9223372036854775807',
+    ): string {
+
+        $min = (int) $min;
+        $max = (int) $max;
+
         if ($min < 0) {
             throw new InvalidArgumentException('"min" should not be lower than 0');
         }
@@ -21,7 +36,7 @@ final readonly class Random
         }
 
         try {
-            return random_int($min, $max);
+            return (string) random_int($min, $max);
         } catch (RandomException $error) {
             throw new InvalidArgumentException('Something went wrong', 0, $error);
         }
